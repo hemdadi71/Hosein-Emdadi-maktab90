c3d5667b2142f0f5b260177356853f0ca2ffaa93
Hosein Emdadi <hemdadi71@gmail.com>
2023-01-22 17:15:38 +0330

    Feature/arrowfunction completed

--- Hw-5.js
+++ Hw-5.js
@@ -1,1 +1,17 @@
 'use strict';
+const ask = (question, yes, no) => {
+  if (confirm(question)) {
+    return yes();
+  } else {
+    return no();
+  }
+};
+ask(
+  'Do you agree',
+  () => {
+    alert('You agreed.');
+  },
+  () => {
+    alert('You canceled the execution.');
+  }
+);

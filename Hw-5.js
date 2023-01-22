d597f08e24f0ddcc60c9156c235c423e5d22a078
Hosein Emdadi <hemdadi71@gmail.com>
2023-01-22 17:13:58 +0330

    Feature/Array_of_Object_Sort completed

--- Hw-5.js
+++ Hw-5.js
@@ -1,1 +1,24 @@
 'use strict';
+const array = [
+  {
+    name: 'ali',
+    age: 25,
+  },
+  {
+    name: 'mohsen',
+    age: 20,
+  },
+  {
+    name: 'hosein',
+    age: 29,
+  },
+  {
+    name: 'mohammad',
+    age: 10,
+  },
+];
+const sortByAge = arr => {
+  arr.sort((a, b) => a.age - b.age);
+  return arr;
+};
+console.log(sortByAge(array));

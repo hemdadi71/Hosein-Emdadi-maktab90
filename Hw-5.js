bc17bee1291659e942e8d4e9fe77049aa200bb61
Hosein Emdadi <hemdadi71@gmail.com>
2023-01-22 17:18:24 +0330

    Feature/notification completed

--- Hw-5.js
+++ Hw-5.js
@@ -1,1 +1,9 @@
 'use strict';
+const notification = document.getElementById('notification');
+const showNotification = (top, right, html, className) => {
+  notification.style.top = `${top}rem`;
+  notification.style.right = `${right}rem`;
+  notification.innerHTML = `${html}`;
+  notification.classList.add(`${className}`);
+};
+showNotification(10, 30, 'Hello', 'hover');

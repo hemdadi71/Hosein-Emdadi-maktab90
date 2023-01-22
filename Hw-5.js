6c5726d3e5093ff7c5ecba6618d4844fdf5f8e16
Hosein Emdadi <hemdadi71@gmail.com>
2023-01-22 18:24:11 +0330

    add object solution to Feature/notification

--- Hw-5.js
+++ Hw-5.js
@@ -7,3 +7,18 @@ const showNotification = (top, right, html, className) => {
   notification.classList.add(`${className}`);
 };
 showNotification(10, 30, 'Hello', 'hover');
+
+
+//object solution
+// const showNotification = ({ top, right, html, className }) => {
+//   notification.style.top = `${top}rem`;
+//   notification.style.right = `${right}rem`;
+//   notification.innerHTML = `${html}`;
+//   notification.classList.add(`${className}`);
+// };
+// showNotification({
+//   top: 10,
+//   right: 10,
+//   html: 'Hello',
+//   // className: 'hover',
+// });

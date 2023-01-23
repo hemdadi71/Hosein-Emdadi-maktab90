88e8fdbc50db7f70349451c15270e7aefee393bf
Hosein Emdadi <hemdadi71@gmail.com>
2023-01-23 18:59:03 +0330

    Feature/pedometer completed

--- Hw-5.js
+++ Hw-5.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const Pedometer = {
+  step: 0,
+  reset() {
+    this.step = 0;
+    return this;
+  },
+  Increase() {
+    this.step++;
+    return this;
+  },
+  Decrease() {
+    this.step--;
+    return this;
+  },
+  read() {
+    console.log(this.step);
+    return this;
+  },
+};
+// Pedometer.Increase().read().reset().Decrease().read().reset().read();
